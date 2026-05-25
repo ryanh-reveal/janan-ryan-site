5a38d45e193678c89cb4da42726afb8ac63762b0
Ryan Heitmann <ryan.heitmann@reveal.nz>
2026-05-25 22:23:30 +1200

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ryanh-reveal.github.io',
-	base: '/janan-ryan-site',
+	site: 'https://jananandryan.info',
+	base: '/',
 });
